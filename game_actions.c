0fcbb1778600ecb9e5d4acaffeae0656f35fb633
CarmenGomezE <159149810+CarmenGomezE@users.noreply.github.com>
2024-02-12 19:33:01 +0100

    Update game_actions.c

--- game_actions.c
+++ game_actions.c
@@ -69,6 +69,24 @@ void game_actions_next(Game *game);
 void game_actions_back(Game *game);
 
 /**
+ * @brief It recives a pointer to Game in the case that the command that the function that calls this one is take,
+ * and checks if the player and the object are in the same location, and if they are, the player takes the object.
+ * @author Estefania Fenoy Montes, Carmen Gómez Escobar
+ *
+ * @param game a pointer to the struct called Game.
+ */
+void game_actions_take(Game *game);
+
+/**
+ * @brief It recives a pointer to Game in the case that the command that the function that calls this one is drop,
+ * the function makes the player drop the object.
+ * @author Estefania Fenoy Montes, Carmen Gómez Escobar
+ *
+ * @param game a pointer to the struct called Game.
+ */
+void game_actions_drop(Game *game);
+
+/**
    Game actions implementation
 */
 
