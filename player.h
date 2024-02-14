bf28c8ad69300e2de079789e6760a97ea6d427ce
CarmenGomezE <159149810+CarmenGomezE@users.noreply.github.com>
2024-02-14 11:35:19 +0100

    Update player.h

--- player.h
+++ player.h
@@ -2,7 +2,7 @@
  * @brief It defines the game interface for the creation, destruction and control of the players
  *
  * @file player.h
- * @author Estefania Fenoy Montes, Carmen Gómez Escobar
+ * @author Carmen Gómez Escobar
  * @version 3.5
  * @date 14-02-2024
  * @copyright GNU Public License
@@ -25,7 +25,7 @@ typedef struct _Player Player;
  * @author Carmen Gómez Escobar
  *
  * @param id the identification number of the player which is going to be created
- * @return a new player and its variables initialized
+ * @return a pointer to a new player and its variables initialized
  */
 Player *player_create(Id id);
 
@@ -34,8 +34,7 @@ Player *player_create(Id id);
  * @author Carmen Gómez Escobar
  *
  * @param player a pointer to the player that will be destroyed
- * @return OK, if everything went well
- * @return ERROR, if it was some mistake
+ *  @return OK if everything worked, ERROR if something went wrong
  */
 Status player_destroy(Player *player);
 
@@ -44,7 +43,7 @@ Status player_destroy(Player *player);
  * @author Carmen Gómez Escobar
  *
  * @param player a pointer to the player in order to access to the ID
- * @return the ID of player
+ * @return the id of the player
  */
 Id player_get_id(Player *player);
 
@@ -54,8 +53,7 @@ Id player_get_id(Player *player);
  *
  * @param player a pointer to the player
  * @param name a string with the name of the player
- * @return OK, if everything went welBooll
- * @return ERROR, if it was some mistake
+ * @return OK if everything worked, ERROR if something went wrong
  */
 Status player_set_name(Player *player, char *name);
 
@@ -64,7 +62,7 @@ Status player_set_name(Player *player, char *name);
  * @author Carmen Gómez Escobar
  *
  * @param player a pointer to the player that allows access to its name
- * @return  a string with the name of the player
+ * @return a string with the name of the player
  */
 const char *player_get_name(Player *player);
 
@@ -93,7 +91,7 @@ Id player_get_location(Player *player);
  *
  * @param player a pointer to the player that allows access to its name
  * @param id a long variable with the position
- * @return OK if everything worked, ERROR if something went wron
+ * @return OK if everything worked, ERROR if something went wrong
  */
 Status player_set_object(Player *player, Id object);
 
@@ -102,7 +100,7 @@ Status player_set_object(Player *player, Id object);
  * @author Carmen Gómez Escobar
  *
  * @param player a pointer to the player that allows access to its name
- * @return FALSE if there is no object, TRUE if there is an object
+ * @return the id of the object
  */
 Id player_get_object(Player *player);
 
@@ -112,8 +110,7 @@ Id player_get_object(Player *player);
  *
  * This function shows the information of player: id and name of the space.
  * @param player a pointer to the palyer
- * @return OK, if everything went well
- * @return ERROR, if it was some mistake
+ *  @return OK if everything worked, ERROR if something went wrong
  */
 Status player_print(Player *player);
 
