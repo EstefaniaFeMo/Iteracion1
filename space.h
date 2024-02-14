073c9cbf75e94f7ca9acc37701a3bf78fbec18b8
CarmenGomezE <159149810+CarmenGomezE@users.noreply.github.com>
2024-02-14 11:14:58 +0100

    Update space.h

--- space.h
+++ space.h
@@ -2,7 +2,7 @@
  * @brief It defines the space interface
  *
  * @file space.h
- * @author Profesores PPROG
+ * @author Profesores PPROG, Estefanía Fenoy Montes
  * @version 3.5
  * @date 26-01-2024
  * @copyright GNU Public License
@@ -17,7 +17,7 @@ typedef struct _Space Space;
 
 /**
  * @brief It creates a new space, allocating memory and initializing its members
- * @author Profesores PPROG
+ * @author Profesores PPROG, Estefanía Fenoy Montes
  *
  * @param id the identification number for the new space
  * @return a new space, initialized
@@ -26,7 +26,7 @@ Space* space_create(Id id);
 
 /**
  * @brief It destroys a space, freeing the allocated memory
- * @author Profesores PPROG
+ * @author Profesores PPROG, Estefanía Fenoy Montes
  *
  * @param space a pointer to the space that must be destroyed
  * @return OK, if everything goes well or ERROR if there was some mistake
@@ -35,7 +35,7 @@ Status space_destroy(Space* space);
 
 /**
  * @brief It gets the id of a space
- * @author Profesores PPROG
+ * @author Profesores PPROG, Estefanía Fenoy Montes
  *
  * @param space a pointer to the space
  * @return the id of space
@@ -44,7 +44,7 @@ Id space_get_id(Space* space);
 
 /**
  * @brief It sets the name of a space
- * @author Profesores PPROG
+ * @author Profesores PPROG, Estefanía Fenoy Montes
  *
  * @param space a pointer to the space
  * @param name a string with the name to store
@@ -54,7 +54,7 @@ Status space_set_name(Space* space, char* name);
 
 /**
  * @brief It gets the name of a space
- * @author Profesores PPROG
+ * @author Profesores PPROG, Estefanía Fenoy Montes
  *
  * @param space a pointer to the space
  * @return  a string with the name of the space
@@ -63,7 +63,7 @@ const char* space_get_name(Space* space);
 
 /**
  * @brief It sets the id of the space located at the north
- * @author Profesores PPROG
+ * @author Profesores PPROG, Estefanía Fenoy Montes
  *
  * @param space a pointer to the space
  * @param id the id number of the space located at the north
@@ -74,7 +74,7 @@ Status space_set_north(Space* space, Id id);
 
 /**
  * @brief It gets the id of the space located at the north
- * @author Profesores PPROG
+ * @author Profesores PPROG, Estefanía Fenoy Montes
  *
  * @param space a pointer to the space
  * @return the id number of the space located at the north
@@ -83,7 +83,7 @@ Id space_get_north(Space* space);
 
 /**
  * @brief It sets the id of the space located at the south
- * @author Profesores PPROG
+ * @author Profesores PPROG, Estefanía Fenoy Montes
  *
  * @param space a pointer to the space
  * @param id the id number of the space located at the south
@@ -93,7 +93,7 @@ Status space_set_south(Space* space, Id id);
 
 /**
  * @brief It gets the id of the space located at the south
- * @author Profesores PPROG
+ * @author Profesores PPROG, Estefanía Fenoy Montes
  *
  * @param space a pointer to the space
  * @return the id number of the space located at the south
@@ -102,7 +102,7 @@ Id space_get_south(Space* space);
 
 /**
  * @brief It sets the id of the space located at the east
- * @author Profesores PPROG
+ * @author Profesores PPROG, Estefanía Fenoy Montes
  *
  * @param space a pointer to the space
  * @param id the id number of the space located at the east
@@ -112,7 +112,7 @@ Status space_set_east(Space* space, Id id);
 
 /**
  * @brief It gets the id of the space located at the east
- * @author Profesores PPROGobject.h player.h game_reader.h
+ * @author Profesores PPROG, Estefanía Fenoy Montes
  * @param space a pointer to the space
  * @param id the id number of the space located at the west
  * @return OK, if everything goes well or ERROR if there was some mistake
@@ -121,7 +121,7 @@ Status space_set_west(Space* space, Id id);
 
 /**
  * @brief It gets the id of the space located at the west
- * @author Profesores PPROG
+ * @author Profesores PPROG, Estefanía Fenoy Montes
  *
  * @param space a pointer to the space
  * @return the id number of the space located at the west
@@ -130,7 +130,7 @@ Id space_get_west(Space* space);
 
 /**
  * @brief It sets the ID of hte object in the space
- * @author Profesores PPROG
+ * @author Profesores PPROG, Estefanía Fenoy Montes
  *
  * @param space a pointer to the space
  * @param value a boolean, specifying if in the space there is an object (TRUE) or not (FALSE)
@@ -140,7 +140,7 @@ Status space_set_object(Space* space, Id object);
 
 /**
  * @brief It gets the ID of the space where object is
- * @author Profesores PPROG
+ * @author Profesores PPROG, Estefanía Fenoy Montes
  *
  * @param space a pointer to the space
  * @return the ID of the object which is in the space
@@ -149,7 +149,7 @@ Id space_get_object(Space* space);
 
 /**
  * @brief It prints the space information
- * @author Profesores PPROG
+ * @author Profesores PPROG, Estefanía Fenoy Montes
  *
  * This fucntion shows the id and name of the space, the spaces that surrounds it and wheter it has an object or not.
  * @param space a pointer to the space
